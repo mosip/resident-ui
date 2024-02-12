a8c36d0e84362e479a04e45191bde49cfa31aba1
Jayesh Kharode <jayesh.kharode@technoforte.co.in>
2024-02-12 14:02:27 +0530

    MOSIP-31552
    Signed-off-by: Jayesh Kharode <jayesh.kharode@technoforte.co.in>

--- residenttest/src/main/java/io/mosip/testrig/residentui/kernel/util/ConfigManager.java
+++ residenttest/src/main/java/io/mosip/testrig/residentui/kernel/util/ConfigManager.java
@@ -90,7 +90,7 @@ public class ConfigManager {
 	private static String HIBERNATE_CONTEXT_CLASS = "hibernate.current_session_context_class";
 
 	private static String AUDIT_DB_USER = "db-su-user";
-	private static String AUDIT_DB_PASS = "postgresql-password";
+	private static String AUDIT_DB_PASS = "postgres-password";
 	private static String AUDIT_DB_SCHEMA = "audit_db_schema";
 	private static String HierarchyLevel0 = "hierarchyLevel0";
 	private static String HierarchyLevel1 = "hierarchyLevel1";
@@ -99,19 +99,19 @@ public class ConfigManager {
 	private static String HierarchyLevel4 = "hierarchyLevel4";
 
 	private static String IDA_DB_USER = "db-su-user";
-	private static String IDA_DB_PASS = "postgresql-password";
+	private static String IDA_DB_PASS = "postgres-password";
 	private static String IDA_DB_SCHEMA = "ida_db_schema";
 
 	private static String PMS_DB_USER = "db-su-user";
-	private static String PMS_DB_PASS = "postgresql-password";
+	private static String PMS_DB_PASS = "postgres-password";
 	private static String PMS_DB_SCHEMA = "pms_db_schema";
 
 	private static String KM_DB_USER = "db-su-user";
-	private static String KM_DB_PASS = "postgresql-password";
+	private static String KM_DB_PASS = "postgres-password";
 	private static String KM_DB_SCHEMA = "km_db_schema";
 
 	private static String MASTER_DB_USER = "db-su-user";
-	private static String MASTER_DB_PASS = "postgresql-password";
+	private static String MASTER_DB_PASS = "postgres-password";
 	private static String MASTER_DB_SCHEMA = "master_db_schema";
 
 	private static String IAM_EXTERNAL_URL = "keycloak-external-url";
